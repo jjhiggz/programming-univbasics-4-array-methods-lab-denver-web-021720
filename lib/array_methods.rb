f11de75d04ce8b1e1acee7f6b90ab1cb096cce39
Jonathan Higger <jonathan.higger@gmail.com>
2020-01-21 21:20:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,23 +3,23 @@ return array,include?(element)
 end
 
 def using_sort(array)
-
+return array.sort
 end
 
 def using_reverse(array)
-
+return array.reverse
 end
 
 def using_first(array)
-
+return array.firt
 end
 
 def using_last(array)
-
+return array[-1]
 end
 
 def using_size(array)
-
+returnarray.size
 end
 array = ['c','b','a']
 element = 'd'
